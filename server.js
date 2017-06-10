5c777e704f6b933ea9be99e472e749212dd010b0
himanshu131194 <himanshu131194@outlook.com>
2017-06-10 16:31:06 +0530

    port

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname+'/public'));
 
-
-app.listen(3000);
+app.listen(app.get('port'));
